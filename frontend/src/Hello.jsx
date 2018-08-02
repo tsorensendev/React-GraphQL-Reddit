33c99c65aa3266c1b6c2c3c89a6052b21c157736
Tanner <tallen904@gmail.com>
2018-08-02 16:30:00 -0700

    Added files

--- frontend/src/Hello.jsx
+++ frontend/src/Hello.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import { ApolloProvider } from 'react-apollo';
+import ApolloProvider from 'react-apollo';
 import { ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
 import ApolloClient from 'apollo-client';
 import { createBrowserHistory } from 'history';
 import indexRoutes from './routes/index';
 
-// Apollo Client Setup
 const hist = createBrowserHistory();
 const httpLink = new HttpLink({
   uri: 'http://localhost:3001/graphql',
@@ -29,20 +28,11 @@ const client = new ApolloClient({
 });
 
 const App = () => {
+  console.log(ApolloProvider)
   return (
-    <ApolloProvider client={client}>
-      <Router history={hist}>
-        <Switch>
-          {indexRoutes.map(route => (
-            <Route
-              path={route.path}
-              key={route.path}
-              component={route.component}
-            />
-          ))}
-        </Switch>
-      </Router>
-    </ApolloProvider>
+    <div>
+      
+    </div>
   );
 };
 
